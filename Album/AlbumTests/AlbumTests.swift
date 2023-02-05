dfc92e4b70d76dbeb6351f3c44671011c6554c44
bonf <apwierk2451@gmail.com>
2023-02-05 16:24:53 +0900

    test: AlbumManager 메소드 테스트
    - Album 데이터 테스트

--- Album/AlbumTests/AlbumTests.swift
+++ Album/AlbumTests/AlbumTests.swift
@@ -9,28 +9,48 @@ import XCTest
 @testable import Album
 
 final class AlbumTests: XCTestCase {
-
+    var sut: AlbumManager!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = AlbumManager.shared
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_recents앨범명이_잘나오는지_확인() {
+        // given
+        sut.requestImageCollection()
+        // when
+        let result = sut.getAlbums().first?.name
+        // then
+        XCTAssertEqual(result, "Recents")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_favorites앨범명이_잘나오는지_확인() {
+        // given
+        sut.requestImageCollection()
+        // when
+        let result = sut.getAlbums()[1].name
+        // then
+        XCTAssertEqual(result, "Favorites")
     }
-
+    
+    func test_recents앨범_사진갯수_잘나오는지_확인() {
+        // given
+        sut.requestImageCollection()
+        // when
+        let result = sut.getAlbums().first?.album.count
+        // then
+        XCTAssertEqual(result, 6)
+    }
+    
+    func test_favorites앨범_사진갯수_잘나오는지_확인() {
+        // given
+        sut.requestImageCollection()
+        // when
+        let result = sut.getAlbums()[1].count
+        // then
+        XCTAssertEqual(result, 1)
+    }
 }
